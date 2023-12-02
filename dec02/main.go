93d0e0f987aae93caaf38a262b825999599be01a
Nick Shine <shine.nick@gmail.com>
2023-12-02 00:23:15 -0600

    clean up day2

--- dec02/main.go
+++ dec02/main.go
@@ -19,89 +19,95 @@ type cubes struct {
 	blue  int
 }
 
+type switchFunc func(in cubes, count int, color string) cubes
+
+var inputRE = regexp.MustCompile(`^Game (\d+): (.*)$`)
+
+func parseSet(in string, f switchFunc) cubes {
+	var out cubes
+	set := strings.Split(in, ",")
+	for _, sv := range set {
+		sv = strings.TrimSpace(sv)
+		svs := strings.Split(sv, " ")
+		if len(svs) != 2 {
+			panic("bad input")
+		}
+		amount, color := svs[0], svs[1]
+		count, _ := strconv.Atoi(amount)
+
+		out = f(out, count, color)
+	}
+
+	return out
+}
+
+func isPossible(setstring string, bag cubes) bool {
+	sets := strings.Split(setstring, ";")
+	for _, s := range sets {
+		set := parseSet(s, func(c cubes, count int, color string) cubes {
+			switch color {
+			case "red":
+				c.red = count
+			case "blue":
+				c.blue = count
+			case "green":
+				c.green = count
+
+			default:
+				panic("invalid input")
+			}
+
+			return c
+		})
+
+		if bag.red < set.red || bag.green < set.green || bag.blue < set.blue {
+			return false
+		}
+	}
+
+	return true
+}
+
 func part1(input []string) int {
-	var inputRE = regexp.MustCompile(`^Game (\d+): (.*)$`)
 	bag := cubes{red: 12, green: 13, blue: 14}
 	var sum int
 
-OUTER:
 	for _, line := range input {
 		res := inputRE.FindStringSubmatch(line)
 		id, _ := strconv.Atoi(res[1])
-		setstring := res[2]
-		cubeSetStrings := strings.Split(setstring, ";")
-		for _, s := range cubeSetStrings {
-			set := strings.Split(s, ",")
-			draw := cubes{}
-			for _, sv := range set {
-				sv = strings.TrimSpace(sv)
-				svs := strings.Split(sv, " ")
-				if len(svs) != 2 {
-					panic("bad input")
-				}
-				amount, color := svs[0], svs[1]
-				count, _ := strconv.Atoi(amount)
-
-				switch color {
-				case "red":
-					draw.red = count
-				case "blue":
-					draw.blue = count
-				case "green":
-					draw.green = count
-
-				default:
-					panic("invalid input")
-				}
-			}
-
-			if bag.red < draw.red || bag.green < draw.green || bag.blue < draw.blue {
-				// game not possible
-				continue OUTER
-			}
+		if isPossible(res[2], bag) {
+			sum += id
 		}
-		sum += id
 	}
 
 	return sum
 }
 
 func part2(input []string) int {
-	var inputRE = regexp.MustCompile(`^Game (\d+): (.*)$`)
 	var sum int
 
 	for _, line := range input {
 		res := inputRE.FindStringSubmatch(line)
 		setstring := res[2]
-		cubeSetStrings := strings.Split(setstring, ";")
+		sets := strings.Split(setstring, ";")
 
 		// for each cubeset, find the max of each color - that is the minimum bag
 		maxR, maxG, maxB := 0, 0, 0
-		for _, s := range cubeSetStrings {
-			set := strings.Split(s, ",")
-			draw := cubes{}
-			for _, sv := range set {
-				sv = strings.TrimSpace(sv)
-				svs := strings.Split(sv, " ")
-				if len(svs) != 2 {
-					panic("bad input")
-				}
-				amount, color := svs[0], svs[1]
-				count, _ := strconv.Atoi(amount)
-
+		for _, s := range sets {
+			parseSet(s, func(c cubes, count int, color string) cubes {
 				switch color {
 				case "red":
-					draw.red = count
+					c.red = count
 					if count > maxR {
 						maxR = count
 					}
 				case "blue":
-					draw.blue = count
+					c.blue = count
 					if count > maxB {
 						maxB = count
 					}
 				case "green":
-					draw.green = count
+					c.green = count
 					if count > maxG {
 						maxG = count
 					}
@@ -109,7 +115,8 @@ func part2(input []string) int {
 				default:
 					panic("invalid input")
 				}
-			}
+				return c
+			})
 		}
 		sum += maxR * maxG * maxB
 	}
