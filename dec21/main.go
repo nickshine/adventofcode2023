46fda419a6575134c33f13df2f9607b997fad9fd
Nick Shine <shine.nick@gmail.com>
2023-12-21 14:09:07 -0600

    day 21 part 2

--- dec21/main.go
+++ dec21/main.go
@@ -6,17 +6,13 @@ import (
 	"strings"
 )
 
-func parseInput() (grid [][]rune, start point) {
+func parseInput() (grid []string, start point) {
 	raw, _ := os.ReadFile(os.Args[1])
-	rows := strings.Split(strings.Trim(string(raw), "\n"), "\n")
+	grid = strings.Split(strings.Trim(string(raw), "\n"), "\n")
 
-	grid = make([][]rune, len(rows))
-
-	for y, row := range rows {
-		grid[y] = make([]rune, len(row))
-		for x, c := range row {
-			grid[y][x] = c
-			if c == 'S' {
+	for y := range grid {
+		for x := range grid[y] {
+			if grid[y][x] == 'S' {
 				start = point{x, y}
 			}
 		}
@@ -38,11 +34,46 @@ type state struct {
 	step int
 }
 
-func part1(grid [][]rune, start point) int {
+func dfs(grid []string, p point, step, maxStep int, seen map[state]struct{}) int {
 
-	// can be any plot that is even number steps away, up to 64 away
-	const MAXSTEP = 64
+	if step == maxStep {
+		return 1
+	}
 
+	x, y := p.x, p.y
+	choices := []point{
+		{x: x, y: y - 1}, // up
+		{x: x, y: y + 1}, // down
+		{x: x - 1, y: y}, // left
+		{x: x + 1, y: y}, // right
+	}
+
+	total := 0
+	n := len(grid)
+	for _, c := range choices {
+		// if c.y < 0 || c.y >= len(grid) || c.x < 0 || c.x >= len(grid[0]) {
+		// 	continue
+		// }
+
+		// go modulo can have negatives, so need extra % - https://github.com/golang/go/issues/448#issuecomment-66049769
+		if grid[(c.y%n+n)%n][(c.x%n+n)%n] == '#' {
+			continue
+		}
+
+		point := point{c.x, c.y}
+		state := state{point, step}
+		if _, ok := seen[state]; ok {
+			continue
+		}
+
+		seen[state] = struct{}{}
+		total += dfs(grid, point, step+1, maxStep, seen)
+	}
+
+	return total
+}
+
+func bfs(grid []string, start point, maxStep int) int {
 	seen := map[state]struct{}{{start, 0}: {}}
 	evens := map[point]struct{}{start: {}}
 	queue := []state{{start, 0}}
@@ -51,13 +82,12 @@ func part1(grid [][]rune, start point) int {
 		cur := queue[0]
 		queue = queue[1:]
 
-		if cur.step > MAXSTEP {
+		if cur.step > maxStep {
 			continue
 		}
 
 		x, y := cur.p.x, cur.p.y
 
-		// proceed an even number of steps
 		choices := []point{
 			{x: x, y: y - 1}, // up
 			{x: x, y: y + 1}, // down
@@ -65,13 +95,19 @@ func part1(grid [][]rune, start point) int {
 			{x: x + 1, y: y}, // right
 		}
 
+		n := len(grid)
 		for _, p := range choices {
 			// out of bounds
-			if p.y < 0 || p.y >= len(grid) || p.x < 0 || p.x >= len(grid[0]) {
-				continue
-			}
+			// if p.y < 0 || p.y >= len(grid) || p.x < 0 || p.x >= len(grid[0]) {
+			// 	continue
+			// }
 
-			if grid[p.y][p.x] == '#' {
+			// if grid[p.y][p.x] == '#' {
+			// 	continue
+			// }
+
+			// go modulo can have negatives, so need extra % - https://github.com/golang/go/issues/448#issuecomment-66049769
+			if grid[(p.y%n+n)%n][(p.x%n+n)%n] == '#' {
 				continue
 			}
 
@@ -91,10 +127,42 @@ func part1(grid [][]rune, start point) int {
 	}
 
 	return len(evens)
+
 }
 
-func part2(in [][]rune, start point) int {
-	return 0
+func part1(grid []string, start point) int {
+	a := dfs(grid, start, 0, 64, make(map[state]struct{}))
+	b := bfs(grid, start, 64)
+
+	fmt.Println("dfs:", a)
+	fmt.Println("bfs:", b)
+
+	return b
+}
+
+func part2(grid []string, start point) int {
+	const maxSteps = 26501365
+	// s1 := bfs(grid, start, start.y)
+	// s2 := bfs(grid, start, start.y+len(grid))
+	// s3 := bfs(grid, start, start.y+(len(grid)*2))
+	// fmt.Println(s1, s2, s3)
+
+	// Starting point is on an empty row in center of grid (row 65)
+	s1 := dfs(grid, start, 0, start.y, make(map[state]struct{}))
+	s2 := dfs(grid, start, 0, start.y+len(grid), make(map[state]struct{}))
+	s3 := dfs(grid, start, 0, start.y+(len(grid)*2), make(map[state]struct{}))
+
+	// quadratic - x is the number of repeated grids in x direction
+	// ax^2 + bx + c
+	x := maxSteps / len(grid)
+
+	// ((diff between s2 and s3) - (diff between s1 and s2)) / 2
+	// (s3-s2)-(s2-s1)
+	a := (s3 - 2*s2 + s1) / 2
+	b := s2 - s1 - a
+	c := s1
+
+	return (a*x*x + b*x + c)
 }
 
 func main() {
